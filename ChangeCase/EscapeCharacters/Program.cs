d750b58bc578618fff35aeadb8ab33220deeeefd
sammiroy <90281906+sammiroy@users.noreply.github.com>
2022-09-12 16:26:35 -0600

    meta

--- ChangeCase/EscapeCharacters/Program.cs
+++ ChangeCase/EscapeCharacters/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            
+            // Escape characters perform tasks in the middle of a string
+            Console.WriteLine("How \nAre \nYou?\n");
+            Console.WriteLine("How \tAre \tYou?");
+            Console.WriteLine("\"That's cool\"");
         }
     }
 }
