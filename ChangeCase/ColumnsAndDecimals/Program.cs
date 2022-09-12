5552694322da4b8a82f0d975e990a9b68e04268a
sammiroy <90281906+sammiroy@users.noreply.github.com>
2022-09-12 16:09:22 -0600

    Added Case & Column demos

--- ChangeCase/ColumnsAndDecimals/Program.cs
+++ ChangeCase/ColumnsAndDecimals/Program.cs
@@ -4,6 +4,20 @@
     {
         static void Main(string[] args)
         {
+            // Columns - can be used to create alinged columns of data in a console window
+            // first column 20 characters wide
+            // second column 10 characters wide
+            // by default data in the columns is right aligned
+            // to make the data left aligned make the column width negative
+
+            Console.WriteLine("{0, -20:0.0}{1, 10}", 100.4552, 200);
+            Console.WriteLine("{0, -20:0.00}{1, 10}", 100.4552, 200);
+            Console.WriteLine("{0, -20:0.000}{1, 10}", 100.4552, 200);
+            Console.WriteLine("{0, -20}{1, 10}", 100.4552, 200);
+            Console.WriteLine("{0, -20:C}{1, 10:C}", 100.4552, 200);
+
+
+
 
         }
     }
