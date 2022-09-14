1e7ba3ab2f64f84646df1d69ec1f9367e3569aa0
sammiroy <90281906+sammiroy@users.noreply.github.com>
2022-09-14 14:46:51 -0600

    Decision Structures added

---
+++ DecisionStructures/DecisionStructuresDemo04/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace DecisionStructuresDemo04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double weight, height, bmi;
+
+            Console.Write("What is your weight in pounds? ");
+            weight = double.Parse(Console.ReadLine());
+            Console.Write("What is your height in inches? ");
+            height = double.Parse(Console.ReadLine());
+
+            bmi = 703 * weight / (Math.Pow(height, 2));
+
+            if (bmi < 18.5)
+            {
+                Console.WriteLine("underweight");
+            }
+            else if (bmi < 25)
+            {
+                Console.WriteLine("norma");
+            }
+            else if (bmi < 30)
+            {
+                Console.WriteLine("overweight");
+            }else
+            {
+                Console.WriteLine("obese");
+            }
+        }
+    }
+}
