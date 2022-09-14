45ea64bd38aef4f0c1a05e5713d04b4cc4598f1c
sammiroy <90281906+sammiroy@users.noreply.github.com>
2022-09-14 11:36:32 -0600

    Finished all questions

--- SimpleSequenceLogicExercise/QuestionFive/Program.cs
+++ SimpleSequenceLogicExercise/QuestionFive/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create and assign char varible
+            Console.Write("Enter a character: ");
+            char x = char.Parse(Console.ReadLine());
+
+            // Print the shape
+            Console.WriteLine($"{x}{x}{x}");
+            Console.WriteLine("{0}{0,2}", x);
+            Console.WriteLine("{0}{0,2}", x);
+            Console.WriteLine("{0}{0,2}", x);
+            Console.WriteLine($"{x}{x}{x}");
         }
     }
 }
--- SimpleSequenceLogicExercise/QuestionSix/Program.cs
+++ SimpleSequenceLogicExercise/QuestionSix/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace QuestionSix
+﻿using System.Diagnostics.Metrics;
+
+namespace QuestionSix
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Init array
+            char[] letters = new char[3];
+
+            // Assign all 3 postions with a character
+            for(int i = 0; i < 3; i++)
+            {
+                Console.Write("Enter a character: ");
+                letters[i] = char.Parse(Console.ReadLine());
+            }
+
+            // Display array backwards
+            Console.WriteLine($"{letters[2]} {letters[1]} {letters[0]}");
         }
     }
 }
