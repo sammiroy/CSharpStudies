23768f34c2e8bc03ede7fce9c1361f48cc58b82f
sammiroy <90281906+sammiroy@users.noreply.github.com>
2022-09-21 13:41:12 -0600

    null

---
+++ DecisionStructures/DecisionStructureExercises/DecisionStructureExercise2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace DecisionStructureExercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int year;
+            string result;
+
+            Console.Write("Enter year: ");
+            year = int.Parse(Console.ReadLine());
+
+
+            if((year%4 == 0 && year%100 != 0) || year%400 == 0)
+            {
+                result = "Yes.";
+            }
+            else
+            {
+                result = "No.";
+            }
+
+            Console.WriteLine($"Is {year} a leap year? {result}");
+        }
+    }
+}
---
+++ DecisionStructures/DecisionStructureExercises/DecisionStructureExercise3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DecisionStructureExercise3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
