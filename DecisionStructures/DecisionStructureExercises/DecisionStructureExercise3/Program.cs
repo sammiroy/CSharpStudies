13bdadb348a8351290e795e8b8d45db7ec8679fd
sammiroy <90281906+sammiroy@users.noreply.github.com>
2022-09-21 14:47:26 -0600

    Decision structures worked on

--- DecisionStructures/DecisionStructureExercises/DecisionStructureExercise3/Program.cs
+++ DecisionStructures/DecisionStructureExercises/DecisionStructureExercise3/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace DecisionStructureExercise3
+﻿using System.Security.Cryptography;
+
+namespace DecisionStructureExercise3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Rock Paper Scissors vs AI
+
+            // Init variables & random object
+            Random random = new Random();
+
+            int cChoice = random.Next(-1, 2); // The AI picks a random integer between -1 and 1
+            int uChoice;
+
+            Console.Write(
+                "1. Rock\n" +
+                "2. Paper\n" +
+                "3. Scissors\n" +
+                "Enter selection: "
+                );
+            uChoice = int.Parse(Console.ReadLine());
+
+            
+
+
+            
         }
     }
 }
