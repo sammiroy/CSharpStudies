cc908735316192d35b17afc08a2e162b5d0b0a38
sammiroy <90281906+sammiroy@users.noreply.github.com>
2022-09-29 19:08:38 -0600

    worked on the URS more

--- UnicornRescueSociety/UnicornRescueSociety/Program.cs
+++ UnicornRescueSociety/UnicornRescueSociety/Program.cs
@@ -17,16 +17,19 @@
             // Price Variables 
             int donationCost = 0;
             int wallCost = 0;
+            int gateCost = 0;
+            int paintCost = 0;
+            int mealUpgrade = 0;
 
             // Dimension Variables
             int sideWallWidth;
             int backWallWidth;
-            int gateWidth, gateHeight;
+            int gateWidth, gateHeight, gatePrice;
 
             #endregion
             Console.WriteLine("*** WELCOME TO URS REGISTRATION ***");
 
-            #region names
+            #region Names
             // Names of user and corn
             Console.Write("What is your name?: ");
             userName = Console.ReadLine();
@@ -88,6 +91,63 @@
             Console.WriteLine("\n*** GATE STYLE ***");
 
             // Present gate options to user, use switch to determine constant value for gate pricing 
+            Console.Write("What Type of gate would you like?" +
+                "\nW - Wooden - $3 / sqft" +
+                "\nS - Silver - $8 / sqft" +
+                "\nG - Gold - $12 / sqft" +
+                "\nEnter Selection: ");
+            userInput = char.Parse(Console.ReadLine().ToLower());
+
+            switch (userInput)
+            {
+                case 'w':
+                    gatePrice = 3;
+                    break;
+                case 's':
+                    gatePrice = 8;
+                    break;
+                case 'g':
+                    gatePrice = 12;
+                    break;
+                default:
+                    // Default to wood, for testing purposes 
+                    gatePrice = 3;
+                    break;
+            }
+
+            // Caculate gate cost 
+            gateCost = (gateWidth * gateHeight) * gatePrice;//(Base * Height) * Material Price
+            #endregion
+
+            #region Gate Paint
+
+            // if user wants to change the paints then allow them to
+            Console.Write("Would you like to change gate colour (Y/N)?: ");
+            userInput = char.Parse(Console.ReadLine().ToLower());
+            if (userInput == 'y')
+            {
+                Console.Write("Avalible Paints" +
+                    "\n M - Mood paint (changes color based on the unicorns mood) – add $200" +
+                    "\n A - Magic paint (changes color several times a day) – add $300" +
+                    "\n R - Reflective paint (acts like a mirror) – add $150" +
+                    "\nEnter Selection: ");
+                userInput = char.Parse(Console.ReadLine().ToLower());
+
+                // Switch for paint selection
+                switch (userInput)
+                {
+                    case 'm':
+                        paintCost = 200;
+                        break;
+                    case 'a':
+                        paintCost = 300;
+                        break;
+                    case 'r':
+                        paintCost = 150;
+                        break;
+                }
+            }
+
             #endregion
         }
     }
