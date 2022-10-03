f0762a8b3422fa1abd9d2b9b416318b59ab6f508
sammiroy <90281906+sammiroy@users.noreply.github.com>
2022-10-03 16:52:32 -0600

    Skiing Page finished

---
+++ TryParse/TryParseLogic/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace TryParseLogic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // TryParse - Trys to parse to a datatype
+            // if it fails, TryParse returns FALSE (meaning the value cannot be parsed to that datatype)
+            // if it succeeds, TryParse returns TRUE and the parsed value is sent back from TryParse through an output parameter
+
+            string input;// User Input
+            int validInput;// user input as an integer after the TryParse
+
+            Console.Write("Please enter an integer: ");
+            input = Console.ReadLine();
+
+            if (int.TryParse(input, out validInput))
+            {
+                Console.WriteLine($"The value {input} could be parsed to an integer!");
+            } else
+            {
+                Console.WriteLine($"The value {input} could NOT be parsed to an integer!");
+            }
+        }
+    }
+}
---
+++ TryParse/TryParseValidationLoops/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace TryParseValidationLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            byte age;
+            string input;
+
+            Console.Write("What is your age?: ");
+            input = Console.ReadLine();
+
+            // Not operator allows the loop to continue to ask if the age is invalid for a byte
+            while( !byte.TryParse(input, out age) || age < 0 || age > 140)
+            {
+                Console.Write($"{input} is a ridiculous value for an age! What is you ACTUAL age?: ");
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Your age is {age}");
+            
+
+        }
+    }
+}
