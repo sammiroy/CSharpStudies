7ede116433f717e1e71144ddca460a73b966106d
sammiroy <90281906+sammiroy@users.noreply.github.com>
2022-10-06 16:40:01 -0600

    Unicorn Racing Pain

---
+++ UnicornRaceGame/UnicornRaceGame/Program.cs
@@ -0,0 +1,38 @@
+﻿ namespace UnicornRaceGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Variables
+            // Init Variables
+            Random roll = new Random();
+
+            string userInput;
+            bool loop = true;
+            int trackLength;
+            int playerPos, aiPos;
+            int rollResult;
+            int gamesWon, gamesLost, gamesTied;
+            #endregion
+
+            // Welcome the user
+            Console.WriteLine("Welcome to the Unicorn Racing Game!\n");
+
+            // Prompt user for trackLength
+            Console.Write("Please enter a track length: ");
+            userInput = Console.ReadLine();
+
+            // Validate user input
+            while (!int.TryParse(userInput, out trackLength) || trackLength < 1)
+            {
+                Console.Clear();
+                Console.WriteLine("Error: must provide an integer >0 ");
+                Console.Write("Please enter a track length: ");
+                userInput = Console.ReadLine();
+            }
+
+
+        }
+    }
+}
