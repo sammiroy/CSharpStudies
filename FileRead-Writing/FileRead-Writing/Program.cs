2d6f9508cde66d76e33f597ba48bd78234a45d65
sammiroy <90281906+sammiroy@users.noreply.github.com>
2022-10-12 13:14:29 -0600

    null

---
+++ FileRead-Writing/FileRead-Writing/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+namespace FileRead_Writing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
