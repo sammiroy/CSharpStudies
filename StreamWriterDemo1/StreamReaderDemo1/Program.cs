8d80b60e7b8c6df56aed216169142dec810fe3fe
sammiroy <90281906+sammiroy@users.noreply.github.com>
2022-10-17 10:31:27 -0600

    null

---
+++ StreamWriterDemo1/StreamReaderDemo1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace StreamReaderDemo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int sum = 0;
+                int counter = 0;
+                const string DataFilePath = @"..\..\..\marks.txt";
+                using (StreamReader reader = new StreamReader(DataFilePath))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        int currentNumber = int.Parse(line);
+                        sum += currentNumber;
+                        counter++;
+                    }
+                    // Compute the average
+                    double avergae = sum / counter;
+                    Console.WriteLine($"The average of the {counter} marks is {avergae:F1}");
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
