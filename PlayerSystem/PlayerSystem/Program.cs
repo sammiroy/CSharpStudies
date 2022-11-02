3583c38ff78203f36d6b26d780ea4f7eddb88429
sammiroy <sammiroy@outlook.com>
2022-11-02 13:06:53 -0600

    Null

--- PlayerSystem/PlayerSystem/Program.cs
+++ PlayerSystem/PlayerSystem/Program.cs
@@ -10,19 +10,81 @@ internal class Program
         //playerPoints  [100]   [80]    [120]
         //index         0       1       2
 
-        // Display Menu
-        // Get Menu Item (C-Create/D-Delete/L-List all players/X- exit)
-        // Loop until valid menu item is entered
+        //25 element arrays
+        const int MAX_PLAYERS = 25;
+
+        int[] playerNumbers = new int[MAX_PLAYERS];
+
+        string[] playerNames = new string[MAX_PLAYERS];
+
+        int[] playerPoints = new int[MAX_PLAYERS];
 
         // Init Variables
         char menuItem;
-        int num;
-        string input;
-
-
+        int playerCount = 0; // Current number of active players, this is the logical size of the array
+        // Keep looping until menuItem is 'x'
         menuItem = GetMenuItem();
-        num = GetValidInt("Input a positive integer: ");
-        input = GetNonEmptyString("Input valid string: ");
+
+        // Process Menu Item
+        ProcessMenuItem(menuItem, playerNumbers, playerNames, playerPoints, MAX_PLAYERS, playerCount);
+
+
+
+    }
+
+    static void ProcessMenuItem(char menuItem, int[] playerNumbers, string[] playerNames, int[] playerPoints, int maxPlayers, int playerCount)
+    {
+        switch (menuItem)
+        {
+            case 'c':
+                CreatePlayer(playerNumbers, playerNames, playerPoints, maxPlayers, playerCount);
+                break;
+            case 'd':
+                DeletePlayer();
+                break;
+            case 'l':
+                ListPlayers();
+                break;
+        }
+    }
+
+    static void CreatePlayer(int[] playerNumbers, string[] playerNames, int[] playerPoints, int maxPlayers, int playerCount)
+    {
+        // Is there room in the arrays
+        // (not rn) Does the player number already exist?
+        // Add to arrays
+        if (playerCount < maxPlayers)
+        {
+            // Is that player number available?
+
+
+            // Player Number
+            playerNumbers[playerCount] = GetValidInt("Enert a positive number for your player's number: ");
+
+            // Player Name
+            playerNames[playerCount] = GetNonEmptyString("Enter your player's name: ");
+
+            // Player Points
+            playerPoints[playerCount] = GetValidInt("Enter a positive number for your player's points: ");
+
+            playerCount++;
+        }
+
+
+        // Add 1 to playerCount
+        // If no room for another player write to screen "Team already full"
+
+
+    }
+
+    static void DeletePlayer()
+    {
+
+    }
+
+    static void ListPlayers()
+    {
+
     }
 
     static void DisplayMenu()
